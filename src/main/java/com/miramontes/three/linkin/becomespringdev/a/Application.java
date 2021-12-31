b67a795748581b703567e3374a24284b0444b7e0
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-31 13:17:26 -0600

    Pasted files from course

--- src/main/java/com/miramontes/three/linkin/becomespringdev/a/Application.java
+++ src/main/java/com/miramontes/three/linkin/becomespringdev/a/Application.java
@@ -1,13 +1,23 @@
 package com.miramontes.three.linkin.becomespringdev.a;
 
+import com.miramontes.three.linkin.becomespringdev.a.service.GreetingService;
+import com.miramontes.three.linkin.becomespringdev.a.service.OutputService;
+import com.miramontes.three.linkin.becomespringdev.a.service.TimeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	public static void main(String[] args) throws InterruptedException {
+		GreetingService greetingService = new GreetingService("Hello");
+		TimeService timeService = new TimeService(true);
+		OutputService outputService = new OutputService(greetingService, timeService);
+
+		for (int i=0;i<5;i++){
+			outputService.generateOutput("Jesús");
+			Thread.sleep(5000);
+		}
 	}
 
 }
