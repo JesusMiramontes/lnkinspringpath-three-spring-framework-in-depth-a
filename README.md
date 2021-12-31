6a3b8af65e1af1d3e56bde8aed87e6f4f66e29f1
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-31 13:13:22 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# 2) Linkedin Spring Learning Path: Spring: Framework in Depth - A
+## Summary
+Code part of course:https://www.linkedin.com/learning/spring-framework-in-depth-2   
+
+## Topics
+
+
+## Tools
+
+
+### Notes
---
+++ src/test/java/com/miramontes/three/linkin/becomespringdev/a/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miramontes.three.linkin.becomespringdev.a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
