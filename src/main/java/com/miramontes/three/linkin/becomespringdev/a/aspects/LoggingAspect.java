dacff1597949842da7773b671cb3d4d80fdf4ad3
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-31 15:35:16 -0600

    Logging with @Around aspect

--- src/main/java/com/miramontes/three/linkin/becomespringdev/a/aspects/LoggingAspect.java
+++ src/main/java/com/miramontes/three/linkin/becomespringdev/a/aspects/LoggingAspect.java
@@ -1,10 +1,8 @@
 package com.miramontes.three.linkin.becomespringdev.a.aspects;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -36,10 +34,40 @@ public class LoggingAspect {
 //        LOGGER.info(sb.toString());
 //    }
 
-    @AfterReturning(value = "executeLogging()", returning = "returnValue")
-    public void logMethodCall(JoinPoint joinPoint, Object returnValue){
+//    @AfterReturning(value = "executeLogging()", returning = "returnValue")
+//    public void logMethodCall(JoinPoint joinPoint, Object returnValue){
+//        StringBuilder sb = new StringBuilder("Method: ");
+//        sb.append(joinPoint.getSignature().getName());
+//        Object[] args = joinPoint.getArgs();
+//        if(null != args && args.length > 0){
+//            sb.append(" args=[ | ");
+//            Arrays.asList(args).forEach(arg -> {
+//                sb.append(arg).append(" | ");
+//            });
+//            sb.append("]");
+//        }
+//
+//        if(returnValue instanceof Collection){
+//            sb.append(", returning: ").append(((Collection) returnValue).size()).append(" instance(s)");
+//        } else {
+//            sb.append(" returning: ").append(returnValue.toString());
+//        }
+//
+//        LOGGER.info(sb.toString());
+//    }
+
+    @Around(value = "executeLogging()")
+    public Object logMethodCall(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+        Object returnValue = joinPoint.proceed();
+        long totalTime = System.currentTimeMillis() - startTime;
+
         StringBuilder sb = new StringBuilder("Method: ");
         sb.append(joinPoint.getSignature().getName());
+
+        sb.append(" totalTime: ").append(totalTime).append("ms");
+
         Object[] args = joinPoint.getArgs();
         if(null != args && args.length > 0){
             sb.append(" args=[ | ");
@@ -56,5 +84,7 @@ public class LoggingAspect {
         }
 
         LOGGER.info(sb.toString());
+
+        return returnValue;
     }
 }
