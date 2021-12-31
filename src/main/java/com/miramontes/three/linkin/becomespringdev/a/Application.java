fd5215184030ef110152f462f433032d3116e241
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-31 13:31:53 -0600

    Migrated app to use application context and IOC

--- src/main/java/com/miramontes/three/linkin/becomespringdev/a/Application.java
+++ src/main/java/com/miramontes/three/linkin/becomespringdev/a/Application.java
@@ -1,18 +1,20 @@
 package com.miramontes.three.linkin.becomespringdev.a;
 
+import com.miramontes.three.linkin.becomespringdev.a.config.ApplicationConfig;
 import com.miramontes.three.linkin.becomespringdev.a.service.GreetingService;
 import com.miramontes.three.linkin.becomespringdev.a.service.OutputService;
 import com.miramontes.three.linkin.becomespringdev.a.service.TimeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) throws InterruptedException {
-		GreetingService greetingService = new GreetingService("Hello");
-		TimeService timeService = new TimeService(true);
-		OutputService outputService = new OutputService(greetingService, timeService);
+		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		OutputService outputService = context.getBean(OutputService.class);
 
 		for (int i=0;i<5;i++){
 			outputService.generateOutput("Jesús");
